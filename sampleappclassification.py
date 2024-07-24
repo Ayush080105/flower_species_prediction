4eb6a4a8143a0c90341c41d565e8976f54529dc6
Ayush080105 <155186867+Ayush080105@users.noreply.github.com>
2024-07-24 21:51:35 +0530

    Update sampleappclassification.py

--- sampleappclassification.py
+++ sampleappclassification.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import sklearn
 from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
 @st.cache_data
